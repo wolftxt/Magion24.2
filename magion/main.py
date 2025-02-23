183727d563a763d14b80bf5da7275d22981b2fa0
wolftxt <wolfdavid49@gmail.com>
2025-02-23 22:24:03 +0100

    Update main.py

--- magion/main.py
+++ magion/main.py
@@ -6,30 +6,41 @@ from calculateSpeed import calculate
 
 TIME_INTERVAL = 13
 IMAGE_COUNT = 42
-
-cam = Camera()
+MAX_TIME_ELAPSED = 570  # 30 Seconds before the 10 minute mark to ensure the time limit.
 
 start_time = time.time()
-images = [0] * IMAGE_COUNT
 
-speed = 0
-out_of_time = False
 
-for i in range(IMAGE_COUNT):
-    if time.time() - start_time > 570:
-        speed /= i
-        out_of_time = True
-        break
-    cycle_time = time.time()
-    images[i] = cam.take_photo("image" + str(i) + ".jpg")
-    if i > 0:
-        speed += calculate(images[i - 1], images[i])
-    elapsed_time = time.time() - cycle_time
-    sleep_time = max(0, TIME_INTERVAL - elapsed_time)
-    time.sleep(sleep_time)
+# Captures a picture every 13 seconds. In between, it calculates speed based on the captured images.
+def capture_images():
+    cam = Camera()
 
-if not out_of_time:
+    speed = 0
+    images = [0] * IMAGE_COUNT
+
+    for i in range(IMAGE_COUNT):
+        # Makes sure that the program will end before the 10 minute limit.
+        if time.time() - start_time > MAX_TIME_ELAPSED:
+            speed /= i
+            return speed
+        cycle_time = time.time()
+        images[i] = cam.take_photo("image" + str(i) + ".jpg")
+        if i > 0:
+            speed += calculate(images[i - 1], images[i])
+        elapsed_time = time.time() - cycle_time
+        sleep_time = max(0, TIME_INTERVAL - elapsed_time)
+        time.sleep(sleep_time)
+
     speed /= IMAGE_COUNT - 1
+    del cam
+    return speed
 
-del cam
-write_result(speed)
+
+def main():
+    speed = capture_images()
+    write_result(speed)
+
+
+if __name__ == "__main__":
+    main()
+
